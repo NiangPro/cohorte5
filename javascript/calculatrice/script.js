59f7112d50256a2aaec7557d9d771d5e6d557988
NiangPro <niangprogrammeur@gmail.com>
2023-10-02 17:53:01 +0200

    calculatrice

--- javascript/calculatrice/script.js
+++ javascript/calculatrice/script.js
@@ -1,32 +1,74 @@
 let entete = document.getElementById("entete");
-let op = "", valeur1= "";
+let op = "", valeur1= "", valeur2 = "", resultat= "", newOp = "";
 
 entete.innerText = "0";
 
+const calculer = ()=>{
+    if (valeur2) {
+        if (op == "+") {
+            resultat = parseInt(valeur1) + parseInt(valeur2)
+        }else if (op == "-") {
+            resultat = parseInt(valeur1) - parseInt(valeur2)
+        } if (op == "/") {
+            resultat = parseInt(valeur1) / parseInt(valeur2)
+        }else if(op == "x"){
+            resultat = parseInt(valeur1) * parseInt(valeur2)
+        }
+
+        entete.innerText = valeur1 +op + valeur2+ " = "+ resultat;
+    }
+}
 // fonction pour ajouter 
 const ajouter = (valeur)=>{
-
-    if (valeur1 == "") {
-        if (valeur != "0") {
-            valeur1 = valeur;
-            entete.innerText = valeur1;
+    if (resultat) {
+        if (newOp) {
+            valeur2 += valeur;
+        entete.innerText = valeur1 + op +valeur2;
         }
-       
-    }else if(valeur1 == "-"){
-        if (valeur != "0") {
-
-            valeur1 += valeur;
-            entete.innerText = valeur1;
-        }
+        
     }else{
-        valeur1 +=valeur;       
-        entete.innerText = valeur1;
+        if (op) {
+            valeur2 += valeur;
+            entete.innerText = valeur1 + op +valeur2;
+        }else if (valeur1 == "") {
+            if (valeur != "0") {
+                valeur1 = valeur;
+                entete.innerText = valeur1;
+            }
+           
+        }else if(valeur1 == "-"){
+            if (valeur != "0") {
+    
+                valeur1 += valeur;
+                entete.innerText = valeur1;
+            }
+        }else{
+            valeur1 +=valeur;       
+            entete.innerText = valeur1;
+        }
     }
+
+    
 }
 
 // fonction qui permet d'effacer
 const effacer = ()=>{
-    if (op) {
+    if (resultat) {
+        op = ""; 
+        valeur1= ""; 
+        valeur2 = ""; 
+        resultat= ""; 
+        newOp = "";
+        entete.innerText = "0";
+    }else if (valeur2) {
+        if (valeur2.length > 1) {
+            valeur2 = valeur2.substring(0, valeur2.length -1 );
+            entete.innerText = valeur1+op+valeur2;
+        }else{
+            valeur2 = "";
+            entete.innerText = valeur1 + op;
+        }
+    }else if (op) {
         op = "";
         entete.innerText = valeur1;
     }else{
@@ -44,7 +86,14 @@ const effacer = ()=>{
 
 // fonction qui permet d'ajouter un opérateur 
 const operateur = (opt)=>{
-    if (valeur1) {
+    if (resultat) {
+        newOp = opt;
+        valeur1 = resultat;
+        op = newOp;
+        valeur2 = "";
+        resultat = "";
+        entete.innerText = valeur1 + op;
+    }else if (valeur1) {
         op = opt;        
         entete.innerText = valeur1 + op;
     }else{
